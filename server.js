8bdfbd4b92be00042b91b126f1c0750a4035a29d
Anthony Salmeron <anthonysalmerondev@gmail.com>
2019-11-10 14:33:13 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs')//filesystem
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+const server = http.createServer(function(req,res){
+  const page=url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  if(page=="/"){
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }else if(page=="/master.css"){
+    fs.readFile('master.css', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.write(data);
+      res.end();
+    });
+  }else if(page=="/main.js"){
+    fs.readFile('main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else if(page=="/check"){
+    if('word' in params){
+      res.writeHead(200,{'Content-Type': 'application/json'});
+      let wordToCheck = params['word'];
+      let reverse = wordToCheck.split('').reverse().join('');
+      if(wordToCheck===reverse){
+        var answer="yes";
+      }else{
+        var answer="no";
+      }
+      const objToJson = {
+        result : answer
+      }
+      res.end(JSON.stringify(objToJson));
+    }
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+server.listen(8000);
