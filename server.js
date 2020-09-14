ef89f21e00179c06c54776eb9c626c787bcd5c95
Jeffrey <jeffreymoreta@gmail.com>
2020-09-14 07:54:35 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/api') {
+    if('text' in params){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        function palinCheck(str){
+            if(!str){
+                return {answer: "Input is empty"};
+            }else if(str.toLowerCase().split("").reverse().join("") == str.toLowerCase()){
+                return {answer: "Palindrome"};
+            }else{
+                return {answer: "Not a Palindrome"};
+            }
+        }
+        res.end(JSON.stringify(palinCheck(params.text)));
+      }
+    }
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
